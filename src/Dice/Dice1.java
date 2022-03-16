80b1db34ca3cd128c52016f1b1b1e74118fb2aab
김우현 <@DESKTOP-TO3M4NB>
2022-03-16 16:21:58 +0900

    Dice

---
+++ src/Dice/Dice1.java
@@ -0,0 +1,30 @@
+package Dice;
+import java.util.*;
+
+//Using Array
+
+public class Dice1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		//N: 주사위의 갯수, M: 주사위 눈의 합
+		int N = sc.nextInt();
+		int M = sc.nextInt();
+		
+		//경우의 수 크기의 배열 만들기
+		int arr[] = new int[N];
+		
+		//모든 경우의 수
+		int cases = (int)Math.pow(6, N);
+		
+		int index = 0;
+		while (index < cases) {
+			
+			
+			
+			index++;
+		}
+		
+	}
+
+}
